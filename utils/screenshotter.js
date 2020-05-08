e9ef747311f9a20af72651003d04407b3e12ced1
Zortrox <zortroxofdoom@gmail.com>
2020-05-08 07:33:44 -0500

    Screenshotter now works asynchronously!

--- utils/screenshotter.js
+++ utils/screenshotter.js
@@ -5,110 +5,119 @@ const path = require('path')
 const fs = require('fs')
 const chalk = require('chalk')
 const _ = require('lodash')
+const Promise = require('promise');
 const Stopwatch = require('statman-stopwatch');
 const stopwatch = new Stopwatch();
 
+const screenShotter = (urlArray) => {
+    stopwatch.start();
+    let resourceCount = 0;
+    console.log(`screen shotter is working...`);
 
+    const writeFile = (path, data, opts = 'utf8') =>
+    new Promise((resolve, reject) => {
+        fs.writeFile(path, data, opts, (err) => {
+            if (err) reject(err);
+            else resolve();
+        })
+    });
 
-const screenShotter = (urlArray) =>{
-  stopwatch.start()
-  let resourceCount = 0
-  console.log(`screen shotter is working...`)
+    function run(url) {
+        return new Promise((resolveURL, rejectURL) => {
+            const {
+                protocol,
+                slashes,
+                host,
+                query,
+                href,
+                pathname
+            } = URL.parse(url);
 
-  function * run(url) {
+            //const urlPosition = urlArray.indexOf(url);
+            const pathnameLength = pathname.length < 2;
 
-    const {
-      protocol,
-      slashes,
-      host,
-      query,
-      href,
-      pathname
-    } = URL.parse(url);
+            const imageBox = path.join(__dirname, `../imgs/${!pathnameLength ? pathname : host}.png`);
 
-    const urlPosition = urlArray.indexOf(url)
-    const pathnameLength = pathname.length<2
+            //console.log(imageBox);
+            console.log(`Processing ${url}`);
 
-    const imageBox =  path.join(__dirname, `../imgs/${!pathnameLength ? pathname : host}.png`)
+            var nightmare = new Nightmare({
+                show: false,
+                width: 1024,
+                height: 768,
+                enableLargerThanScreen: true
+            });
 
-    console.log(imageBox)
+            nightmare.goto(url)
+                .wait('body')
+                .evaluate(() => {
+                    var body = document.querySelector('body');
+                    return { 
+                        height: body.scrollHeight,
+                        width:body.scrollWidth
+                    }
+                })
+                .then(dimensions => {
+                    nightmare.viewport(dimensions.width, dimensions.height)
+                        .screenshot()
+                        .then( buffer => {
+                            writeFile(imageBox, buffer).finally(data => {
+                                //console.log(chalk.bgCyan.red(`New .png file saved!`));
+                                //console.log(chalk.bgCyan.red(`Did that thing`))
+                                
+                                resourceCount++;
+                                nightmare.end().then(() => {
+                                    resolveURL(chalk.bold.magenta(imageBox));
+                                });
+                            });
+                        })
+                        .catch( e => {
+                            rejectURL(chalk.underline.red(`Something smells borken: ${e}`));
+                        });
+                });
 
-      var nightmare = new Nightmare({
-          show: false,
-          width: 1024,
-          height: 768
-      });
-      var dimensions = yield nightmare.goto(url)
-          .wait('body')
-          .evaluate(function() {
-              var body = document.querySelector('body');
-              return {
-                  height: body.scrollHeight,
-                  width:body.scrollWidth
-              }
-          });
+            
 
-      console.dir(dimensions);
-      yield nightmare.viewport(dimensions.width, dimensions.height)
-          // .wait(1000)
-          .screenshot()
-          .then(buffer=>{
-
-            fs.writeFile(imageBox, buffer, (e)=>{
-              if(e) throw new Error;
-              console.log(chalk.bgCyan.red(`New .png file saved!`))
-            //  console.log(chalk.bgCyan.red(`Did that thing`))
-            })
-
-          })
-          .then(()=>{
-            stopwatch.split()
-            const theSplitTime = stopwatch.splitTime()/1000
-            stopwatch.unsplit()
-            console.log(chalk.bold.magenta(imageBox))
-            console.log(chalk.bgCyan(`Split time: ${theSplitTime} seconds`))}
-          )
-          .catch(e=>{
-            console.log(chalk.underline.red(`Something smells borken: ${e}`))
-          });
-
-      yield nightmare.end();
-  }
-
-
-  async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-      }
+            //console.dir(dimensions);
+        });
     }
 
-  asyncForEach(urlArray, async (url, urlIndex, arrayFull)=>{
+    let promiseArray = [];
+    urlArray.forEach( url => promiseArray.push(run(url)) );
+    Promise.all(promiseArray).then(values => {
+        values.forEach( msg => console.log(msg) );
+    });
+
+    async function asyncForEach(array, callback) {
+        for (let index = 0; index < array.length; index++) {
+            await callback(array[index], index, array);
+        }
+    }
+
+    /*asyncForEach(urlArray, async (url, urlIndex, arrayFull) => {
 
 
-      await vo(run)(url)
-        .then(
-         resourceCount = resourceCount + 1
-       ).catch(e=>{
-         return e
-       })
+        await vo(run)(url)
+            .then(
+                resourceCount = resourceCount + 1
+            ).catch(e => {
+                return e;
+            });
 
- });
+    });*/
 
-
-  process.on('exit', ()=>{
-    console.log(chalk.bgBlue.bold.white('All Done!!'))
-    const stopIt = stopwatch.stop()/1000
-    console.log(chalk.bold.yellow(
-      `
-      **************************
-
-
-      Total: ${resourceCount} pages in ${stopIt} seconds
+    process.on('exit', () => {
+        console.log(chalk.bgBlue.bold.white('All Done!!'));
+        const stopIt = stopwatch.stop() / 1000;
+        console.log(chalk.bold.yellow(
+            `
+          **************************
 
 
-      **************************`))
+          Total: ${resourceCount} pages in ${stopIt} seconds
 
 
+          **************************`));
     });
 
 }
